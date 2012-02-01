715b2e1d9e5151ace14299b45e90fccea3576211
Fabian de Groot <fbdegroot@gmail.com>
2012-02-01 22:55:21 +0100

    Added nuspec file (including automated package creation on build) and updated the assembly version

--- Lithium/Properties/AssemblyInfo.cs
+++ Lithium/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Lithium")]
-[assembly: AssemblyDescription("Lightweight SQL object mapper based on ADO.NET")]
+[assembly: AssemblyDescription("Lightweight SQL object mapper based on dapper-dot-net by Sam Saffron and Marc Gravell")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("de Groot Software")]
 [assembly: AssemblyProduct("Lithium")]
-[assembly: AssemblyCopyright("Copyright © de Groot Software 2011")]
+[assembly: AssemblyCopyright("Copyright © de Groot Software 2011-2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.1.0.1")]
+[assembly: AssemblyFileVersion("0.1.0.1")]
